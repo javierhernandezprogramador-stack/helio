ad83192c9ad90dc26e2aaffa04cb12f6137319db
javierhernandezprogramador-stack <javier.hernandez.programador@gmail.com>
2026-04-30 16:06:15 -0600

    Inicio del proyecto

---
+++ src/main/java/com/hernandezsanchez/dev/helio/HelioApplication.java
@@ -0,0 +1,13 @@
+package com.hernandezsanchez.dev.helio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hernandezsanchez/dev/helio/HelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hernandezsanchez.dev.helio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
